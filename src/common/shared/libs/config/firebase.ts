e5a35e6148ea0120109cc4c948254b82e59266e7
Eirfire <jacob35422@gmail.com>
2022-10-17 06:39:47 +1000

    firebase admin secrets removed

--- src/common/shared/libs/config/firebase.ts
+++ src/common/shared/libs/config/firebase.ts
@@ -1,30 +1,19 @@
-import { initializeApp, getApps, getApp} from 'firebase/app'
-import { getAnalytics } from 'firebase/analytics';
-import { getAuth } from "firebase/auth";
-import { getFirestore, Timestamp, FieldValue } from 'firebase/firestore'
+import admin from 'firebase-admin';
 
 
 
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATA_BASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID
-};
+admin.initializeApp({
+  credential: admin.credential.cert(require('./Meally_firebase_admin.json')),
+  databaseURL: "https://recipe-webapp-a2a71-default-rtdb.asia-southeast1.firebasedatabase.app"
+})
 
-const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
-// Initialize Firebase\
-// let analytics = undefined;
-// if (typeof window !== 'undefined') {
-//   let analytics = getAnalytics(app);
-// }
 
-const db = getFirestore(app);
-const auth = getAuth(app);
+
+
+
+const db = admin.firestore()
+const auth = admin.auth()
+const analytics = admin.an
     
 
 export { db, auth};
