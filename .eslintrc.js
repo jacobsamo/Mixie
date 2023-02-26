6da69c8bd22fb92b0eb569cc25cab0f18bf7767d
Jacob <jacob35422@gmail.com>
2023-02-26 18:02:33 +1000

    FIX: ESLINT rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,13 @@
 module.exports = {
-    root: true,
-    // This tells ESLint to load the config from the package `eslint-config-custom`
-    extends: ["custom"],
-    settings: {
-      next: {
-        rootDir: ["apps/*/"],
-      },
+  root: true,
+  // This tells ESLint to load the config from the package `eslint-config-custom`
+  extends: ["custom"],
+  settings: {
+    next: {
+      rootDir: ["apps/*/"],
     },
-  };
-  
+  },
+  rules: {
+    "@next/next/no-html-link-for-pages": "off",
+  },
+};
--- apps/meally.com.au/.eslintrc.js
+++ apps/meally.com.au/.eslintrc.js
@@ -1,9 +1,20 @@
 module.exports = {
   root: true,
-  extends: ["custom", "next/core-web-vitals"],
+  extends: ['custom', 'next/core-web-vitals'],
   rules: {
-    "@next/next/no-html-link-for-pages": "off",
-    "react/jsx-key": "off",
-    "@next/next/no-page-custom-font": "off"
+    // Disable rules that aren't relevant to your codebase
+    '@next/next/no-html-link-for-pages': 'off',
+    'react/jsx-key': 'off',
+    '@next/next/no-page-custom-font': 'off',
+    '@next/next/no-img-element': 'error',
+
+    // Add any additional rules you need
+    // 'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'off',
+    'import/no-anonymous-default-export': 'off',
+    'turbo/no-undeclared-env-vars': 'warn',
+
+    // Use autofix where possible
+
   },
 };
