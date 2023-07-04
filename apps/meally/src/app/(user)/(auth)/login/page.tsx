6b032782f49dbf1e63898e539bf62e65d890cad4
Jacob <jacob35422@gmail.com>
2023-07-04 19:05:21 +1000

    51-FEAT: add providers into auth config

--- apps/meally/src/app/(user)/(auth)/login/page.tsx
+++ apps/meally/src/app/(user)/(auth)/login/page.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 
 const page = () => {
+  
+
   return (
-    <div>page</div>
+    <div>
+
+
+    </div>
   )
 }
 
--- apps/meally/src/db/next-auth-adapter.ts
+++ apps/meally/src/db/next-auth-adapter.ts
@@ -55,15 +55,18 @@ export const authOptions: NextAuthOptions = {
   },
   adapter: DrizzleAdapter(),
   providers: [
-    /**
-     * ...add more providers here.
-     *
-     * Most other providers require a bit more work than the Discord provider. For example, the
-     * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
-     * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
-     *
-     * @see https://next-auth.js.org/providers/github
-     */
+    GitHubProvider({
+      clientId: env.GITHUB_CLIENT_ID,
+      clientSecret: env.GITHUB_CLIENT_SECRET,
+    }),
+    FacebookProvider({
+      clientId: env.FACEBOOK_APP_ID,
+      clientSecret: env.FACEBOOK_CLIENT_SECRET,
+    }),
+    GoogleProvider({
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
+    }),
   ],
 };
 
