6defcba012a3816afae4e1424b5cfbee603d06c6
Jacob <jacob35422@gmail.com>
2023-10-25 17:41:42 +1000

    70-FIX: turbo

--- apps/meally/next.config.mjs
+++ apps/meally/next.config.mjs
@@ -1,4 +1,9 @@
 import "./env.mjs";
+import { env } from "./env.mjs";
+
+if (env.NODE_ENV === "development") {
+  process.env.VERCEL_URL = "http://localhost:3000";
+}
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
