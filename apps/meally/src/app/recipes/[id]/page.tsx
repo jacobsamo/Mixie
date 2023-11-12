c21ebacfc853f3fbfd30f4618b3c482758cf8a48
Jacob <jacob35422@gmail.com>
2023-11-12 20:56:18 +1000

    HOT-FIX: stop building static paths

--- apps/meally/src/app/recipes/[id]/page.tsx
+++ apps/meally/src/app/recipes/[id]/page.tsx
@@ -9,25 +9,24 @@ import type { Recipe } from "@db/types";
 import { eq } from "drizzle-orm";
 import { Metadata } from "next";
 import { RecipeJsonLd } from "next-seo";
+import { unstable_cache } from "next/cache";
 import { notFound } from "next/navigation";
 
 export const revalidate = 60 * 60;
 
-const getRecipes = async () => {
-  const recipes = await db.query.recipes.findMany({
-    with: { info: true },
-    where: eq(recipeSchema.isPublic, true),
-  });
-  return recipes;
-};
-
-export async function generateStaticParams() {
-  const recipes = await getRecipes();
-
-  return recipes.map((recipe) => ({
-    id: recipe.id,
-  }));
-}
+const getRecipes = unstable_cache(
+  async () => {
+    const recipes = await db.query.recipes.findMany({
+      with: { info: true },
+      where: eq(recipeSchema.isPublic, true),
+    });
+    return recipes;
+  },
+  ["fullRecipes"],
+  {
+    revalidate: 60 * 60,
+  }
+);
 
 export async function generateMetadata({
   params,
