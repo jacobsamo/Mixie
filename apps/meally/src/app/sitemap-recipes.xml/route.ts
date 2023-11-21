edfb2e6a718140bf28907025d52a09166a957277
Jacob <jacob35422@gmail.com>
2023-11-21 20:57:45 +1000

    HOT-FIX: only get recipes that are public

--- apps/meally/src/app/sitemap-recipes.xml/route.ts
+++ apps/meally/src/app/sitemap-recipes.xml/route.ts
@@ -1,8 +1,12 @@
 export const dynamic = "force-dynamic"; // defaults to force-static
 import { db } from "@/src/server/db";
+import { info } from "@/src/server/db/schemas";
+import { eq } from "drizzle-orm";
 
 export async function GET() {
-  const recipes = await db.query.info.findMany({});
+  const recipes = await db.query.info.findMany({
+    where: eq(info.isPublic, true),
+  });
 
   return new Response(`
   <?xml version="1.0" encoding="UTF-8"?>
