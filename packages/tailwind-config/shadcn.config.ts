6b98f968ca542956b533bd26762e9b09657dc3b2
Jacob <jacob35422@gmail.com>
2024-05-09 08:10:47 +1000

    FEAT: Get shadcn implemented properly
    Shadcn is now working excatly how it should be, now ready for migrating styles over to shadcn styles and customisation

--- packages/tailwind-config/shadcn.config.ts
+++ packages/tailwind-config/shadcn.config.ts
@@ -8,30 +8,7 @@ const config = {
     "./app/**/*.{ts,tsx}",
     "./src/**/*.{ts,tsx}",
   ],
-  prefix: "",
   theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
-    },
-    fontFamily: {
-      roboto: [
-        "Roboto",
-        "Helvetica Neue",
-        "-apple-system",
-        "Oxygen",
-        "Ubuntu",
-        "Cantarell",
-        "Fira Sans",
-        "Droid Sans",
-      ],
-      monospace: ["monospace", "sans-serif", "-apple-system"],
-      "sans-serif": ["sans-serif", "serif", "-apple-system"],
-      "open-dyslexic": ["OpenDyslexic", "sans-serif"],
-    },
     extend: {
       colors: {
         border: "hsl(var(--border))",
@@ -74,18 +51,6 @@ const config = {
         yellow: "hsl(42, 100%, 54%)",
         peach: "hsl(351, 65%, 64%)",
         red: "hsl(355, 78%, 56%)",
-      },
-      fontSize: {
-        "step--4": "0.82rem",
-        "step--3": "1.02rem",
-        "step--2": "1.28rem",
-        "step--1": "1.6rem",
-        step0: "2rem",
-        step1: "2.50rem",
-        step2: "3.13rem",
-        step3: "3.91rem",
-        step4: "4.88rem",
-        step5: "6.10rem",
       },
       borderRadius: {
         lg: "var(--radius)",
--- packages/tailwind-config/tailwind.config.ts
+++ packages/tailwind-config/tailwind.config.ts
@@ -1,31 +1,64 @@
-import { type Config } from "tailwindcss";
+import type { Config } from "tailwindcss";
 
-export default {
+const config = {
+  darkMode: ["class"],
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
   theme: {
+    fontFamily: {
+      roboto: [
+        "Roboto",
+        "Helvetica Neue",
+        "-apple-system",
+        "Oxygen",
+        "Ubuntu",
+        "Cantarell",
+        "Fira Sans",
+        "Droid Sans",
+      ],
+      monospace: ["monospace", "sans-serif", "-apple-system"],
+      "sans-serif": ["sans-serif", "serif", "-apple-system"],
+      "open-dyslexic": ["OpenDyslexic", "sans-serif"],
+    },
     extend: {
-      fontFamily: {
-        roboto: [
-          "Roboto",
-          "Helvetica Neue",
-          "-apple-system",
-          "Oxygen",
-          "Ubuntu",
-          "Cantarell",
-          "Fira Sans",
-          "Droid Sans",
-        ],
-        monospace: ["monospace", "sans-serif", "-apple-system"],
-        "sans-serif": ["sans-serif", "serif", "-apple-system"],
-        "open-dyslexic": ["OpenDyslexic", "sans-serif"],
-      },
       colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
         black: "hsl(222, 28% 7%)",
         grey: "hsl(224, 19% 12%)",
         white: "hsl(30, 36%, 96%)",
@@ -34,38 +67,40 @@ export default {
         peach: "hsl(351, 65%, 64%)",
         red: "hsl(355, 78%, 56%)",
       },
-    },
-    fontSize: {
-      "step--4": "0.82rem",
-      "step--3": "1.02rem",
-      "step--2": "1.28rem",
-      "step--1": "1.6rem",
-      step0: "2rem",
-      step1: "2.50rem",
-      step2: "3.13rem",
-      step3: "3.91rem",
-      step4: "4.88rem",
-      step5: "6.10rem",
-    },
-    screens: {
-      xs: "320",
-      sm: "640px",
-      md: "768px",
-      lg: "1024px",
-      xl: "1280px",
-      "2xl": "1440px",
-    },
-    boxShadow: {
-      sm: "0 1px 2px 0 rgb(0 0 0 / 0.05)",
-      main: "0px 0px 20px rgba(0, 0, 0, 0.25)",
-      DEFAULT: "0px 0px 20px rgba(0, 0, 0, 0.25)",
-      md: "0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)",
-      lg: "0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1)",
-      xl: "0 20px 25px -5px rgb(0 0 0 / 0.1), 0 8px 10px -6px rgb(0 0 0 / 0.1)",
-      "2xl": "0 25px 50px -12px rgb(0 0 0 / 0.25)",
-      inner: "inset 0 2px 4px 0 rgb(0 0 0 / 0.05)",
-      none: "none",
-      searchBarShadow: "17px 21px 17px rgba(0, 0, 0, 0.25)",
+      fontSize: {
+        "step--4": "0.82rem",
+        "step--3": "1.02rem",
+        "step--2": "1.28rem",
+        "step--1": "1.6rem",
+        step0: "2rem",
+        step1: "2.50rem",
+        step2: "3.13rem",
+        step3: "3.91rem",
+        step4: "4.88rem",
+        step5: "6.10rem",
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
     },
   },
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
+
+export default config;
