da1343fba902a45942f456652f43cdf94a808314
Jacob <jacob35422@gmail.com>
2024-06-27 17:13:35 +1000

    HOT-FIX: env.mjs to have a min 1 so it doesn't fail builds

--- apps/website/env.mjs
+++ apps/website/env.mjs
@@ -11,9 +11,9 @@ export const env = createEnv({
     UPLOADTHING_SECRET: z.string().min(1),
     UPLOADTHING_APP_ID: z.string().min(1),
     SUPABASE_SERVICE_ROLE: z.string().min(1),
-    GOOGLE_AI_API_KEY: z.string(),
-    UPSTASH_REDIS_REST_URL: z.string(),
-    UPSTASH_REDIS_REST_TOKEN: z.string(),
+    GOOGLE_AI_API_KEY: z.string().min(1),
+    UPSTASH_REDIS_REST_URL: z.string().min(1),
+    UPSTASH_REDIS_REST_TOKEN: z.string().min(1),
   },
   client: {
     NEXT_PUBLIC_API_APP_TOKEN: z.string().min(1),
@@ -21,6 +21,8 @@ export const env = createEnv({
     NEXT_PUBLIC_UNSPLASH_ACCESS_KEY: z.string().min(1),
     NEXT_PUBLIC_SUPABASE_URL: z.string().min(1),
     NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string().min(1),
+    NEXT_PUBLIC_POSTHOG_KEY: z.string().min(1),
+    NEXT_PUBLIC_POSTHOG_HOST: z.string().min(1),
   },
   runtimeEnv: {
     // Server
@@ -44,5 +46,7 @@ export const env = createEnv({
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
     NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
     SUPABASE_SERVICE_ROLE: process.env.SUPABASE_SERVICE_ROLE,
+    NEXT_PUBLIC_POSTHOG_KEY: process.env.NEXT_PUBLIC_POSTHOG_KEY,
+    NEXT_PUBLIC_POSTHOG_HOST: process.env.NEXT_PUBLIC_POSTHOG_KEY,
   },
 });
