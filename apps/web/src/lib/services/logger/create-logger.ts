887710c770a43e1c2b8e311f6247341fd5d8e1c1
Jacob <jacob35422@gmail.com>
2024-07-31 18:26:52 +1000

    MIX-170: fix logging hopefully, add logging in preview
    - Add @baselime/pino-transport to serverComponentsExternalPackages to hopefully fix it throwing a transport error
    - Changed it so that errors will be logged in different enviroments.

--- apps/web/src/lib/services/logger/create-logger.ts
+++ apps/web/src/lib/services/logger/create-logger.ts
@@ -4,23 +4,22 @@ import pino from "pino";
 
 // credit to openpanel for thier implementation of a logger: https://github.com/Openpanel-dev/openpanel/blob/3d0474d07b82872798871fc145a1eea299d9ea2f/packages/logger/index.ts
 export function createLogger({ dataset }: { dataset: string }) {
-  const targets: TransportTargetOptions[] =
-    env.NODE_ENV === "production" && env.BASELIME_KEY
-      ? [
-          {
-            target: "@baselime/pino-transport",
-            options: {
-              baselimeApiKey: env.BASELIME_KEY,
-              dataset,
-              service: "mixie-web",
-            },
+  const targets: TransportTargetOptions[] = env.BASELIME_KEY
+    ? [
+        {
+          target: "@baselime/pino-transport",
+          options: {
+            baselimeApiKey: env.BASELIME_KEY,
+            dataset,
+            service: "mixie-web",
           },
-        ]
-      : [
-          {
-            target: "pino-pretty",
-          },
-        ];
+        },
+      ]
+    : [
+        {
+          target: "pino-pretty",
+        },
+      ];
 
   const transport = pino.transport({
     targets,
