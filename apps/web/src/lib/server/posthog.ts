bf73873c7a5a5640ddfe81eef436a772c3b21121
Jacob <jacob35422@gmail.com>
2024-08-12 14:41:45 +1000

    MIX-174: get posthog server to read from env.mjs

--- apps/web/src/lib/server/posthog.ts
+++ apps/web/src/lib/server/posthog.ts
@@ -1,8 +1,9 @@
 // app/posthog.js
+import { env } from "env";
 import { PostHog } from "posthog-node";
 
 export default function PostHogClient() {
-  const posthogClient = new PostHog(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+  const posthogClient = new PostHog(env.NEXT_PUBLIC_POSTHOG_KEY, {
     host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
     flushAt: 1,
     flushInterval: 0,
