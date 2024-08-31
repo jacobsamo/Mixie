6f040f31a83c2998e8791b690b8ab06778532be7
Jacob <jacob35422@gmail.com>
2024-08-31 19:59:08 +1000

    MIX-123: convert state to be using zustand rather than jotai

--- apps/web/src/components/providers/dialogs.tsx
+++ apps/web/src/components/providers/dialogs.tsx
@@ -1,18 +1,8 @@
 "use client";
 import { SearchDialog } from "@/components/search";
-import { atom } from "jotai";
+
 import dynamic from "next/dynamic";
 
-export const createRecipeOpen = atom<boolean>(false);
-export const bookmarkRecipeOpen = atom<{
-  open: boolean;
-  recipe_id: string | null;
-  userId: string | null;
-}>({ open: false, recipe_id: null, userId: null });
-
-export const giveFeedbackOpen = atom<boolean>(false);
-export const searchOpen = atom<boolean>(false);
-export const collectionCreateOpen = atom<boolean>(false);
 
 const CreateRecipeDialog = dynamic(
   () => import("@/components/modals/create-recipe-modal"),
