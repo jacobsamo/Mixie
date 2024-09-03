cfa42735fe2edda2a412c726ab7c7d7119a3ed68
Jacob <jacob35422@gmail.com>
2024-09-03 14:40:47 +1000

    MIX-123: fix context to base64 failing due to removal of export

--- apps/web/src/lib/utils/images.ts
+++ apps/web/src/lib/utils/images.ts
@@ -28,3 +28,35 @@ export const convertBase64ToFile = (
     });
     resolve(file);
   });
+
+export function convertUint8ArrayToBase64(array: Uint8Array): string {
+  let latin1string = "";
+
+  // Note: regular for loop to support older JavaScript versions that
+  // do not support for..of on Uint8Array
+  for (let i = 0; i < array.length; i++) {
+    latin1string += String.fromCodePoint(array[i]!);
+  }
+
+  return btoa(latin1string);
+}
+
+export type DataContent = string | Uint8Array | ArrayBuffer | Buffer;
+
+/**
+Converts data content to a base64-encoded string.
+
+@param content - Data content to convert.
+@returns Base64-encoded string.
+*/
+export function convertDataContentToBase64String(content: DataContent): string {
+  if (typeof content === "string") {
+    return content;
+  }
+
+  if (content instanceof ArrayBuffer) {
+    return convertUint8ArrayToBase64(new Uint8Array(content));
+  }
+
+  return convertUint8ArrayToBase64(content);
+}
